0b76d86dc35a9119bdd28088d9a3153a3e785c9c
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-15 18:52:57 +0200

    feat: ✨ error handling and reactivity with resources

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,21 +13,40 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
 
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  countryResource = resource({
+    params: () => ({ query: this.query() }),
+    loader: async ({ params }) => {
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
+      if (!params.query) return [];
 
-    this.isLoading.set(true);
-    this.isError.set(null);
+      return await firstValueFrom(
+        this.countryService.searchByCapital(params.query)
+      );
+    },
+  });
 
-    this.countryService.searchByCapital(query)
-      .subscribe((countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries)
-      })
-  }
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   this.countryService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries)
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err);
+  //     },
+  //   });
+  // }
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Country } from '../interfaces/country.interface';
@@ -11,14 +11,20 @@ const API_URL = 'https://restcountries.com/v3.1'
   providedIn: 'root'
 })
 export class CountryService {
-  private http = inject(HttpClient)
+  private http = inject(HttpClient);
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)));
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+
+        return throwError(
+          () => new Error(`No se han podido obtener países con esa búsqueda: "${query}"`)
+        );
+      })
+    );
   }
 }
