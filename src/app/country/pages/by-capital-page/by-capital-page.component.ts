9d20a730025e5b824a234fc9270ba41f6227321d
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-16 13:09:16 +0200

    add: ✨ show loading spinner

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -5,10 +5,11 @@ import { of } from 'rxjs';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [SearchInputComponent, CountryListComponent],
+  imports: [SearchInputComponent, CountryListComponent, LoadingSpinnerComponent],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -5,10 +5,11 @@ import { of } from 'rxjs';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-by-country-page',
-  imports: [SearchInputComponent, CountryListComponent],
+  imports: [SearchInputComponent, CountryListComponent, LoadingSpinnerComponent],
   templateUrl: './by-country-page.component.html',
 })
 export class ByCountryPageComponent {
